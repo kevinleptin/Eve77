61dfbab59bef9290fd06404e01ed7d6fbb52ff06
void fourd <fourdvoid@gmail.com>
2018-08-16 12:27:06 +0800

    init IDEA project

---
+++ src/main/java/com/fourdvoid/eve77/Eve77Application.java
@@ -0,0 +1,12 @@
+package com.fourdvoid.eve77;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eve77Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Eve77Application.class, args);
+    }
+}
